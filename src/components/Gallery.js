6b474d35ccc84de33a5072834721784380a5a6fc
Mohemmad Imtiyaz <imtiyajkhan789@gmail.com>
2022-11-07 09:56:18 +0530

    updated zoom

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,14 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import axios from 'axios'
+import { Controlled } from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
+
 const Gallery = () => {
     const [data, setData] = useState()
+    const [isZoom, setIsZoom] = useState(false)
     const { id } = useParams()
     // console.log(id)
     useEffect(() => {
         fetchApi()
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+
     const fetchApi = async () => {
         try {
             const data = await axios(`https://jsonplaceholder.typicode.com/albums/${id}/photos`)
@@ -20,6 +25,11 @@ const Gallery = () => {
         }
 
     }
+
+    const handleZoomChange = useCallback(shouldZoom => {
+        setIsZoom(shouldZoom)
+    }, [])
+
     return (
         <div className='container-fluid d-flex flex-wrap justify-content-evenly p-5'>
             {data && data.map((data, id) => {
@@ -30,7 +40,9 @@ const Gallery = () => {
                                 <div className="card  m-3 shadow p-3  bg-body rounded gallery_card"  >
                                     {/* <img src="." key={id} className="card-img-top" alt="..." /> */}
                                     <div className="card-body">
-                                        <img src={`https://picsum.photos/id/${id}/${200}/${200}`} key={id} className="card-img-top" alt="..." />
+                                        <Controlled isZoomed={isZoom} onZoomChange={handleZoomChange} >
+                                            <img src={`https://picsum.photos/id/${id}/${200}/${200}`} key={id} className="card-img-top" alt="..." />
+                                        </Controlled>
                                         <h5 className="card-title">{data.title}</h5>
 
                                         {/* <h6 className="card-title">{data.username}</h6> */}
